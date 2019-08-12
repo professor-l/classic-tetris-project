8b6dc11ca2f0f495a973e4c8313911c6b481b647
Elle Nolan <ellemnolan@gmail.com>
2019-08-12 12:15:33 -0400

    Use Match and Game models in queue commands

--- classic_tetris_project/models/matches.py
+++ classic_tetris_project/models/matches.py
@@ -4,11 +4,11 @@ from .users import User, TwitchUser
 
 
 class Match(models.Model):
-    player1 = models.ForeignKey(User, on_delete=models.PROTECT)
-    player2 = models.ForeignKey(User, on_delete=models.PROTECT)
+    player1 = models.ForeignKey(User, on_delete=models.PROTECT, related_name="+")
+    player2 = models.ForeignKey(User, on_delete=models.PROTECT, related_name="+")
     wins1 = models.IntegerField(default=0)
     wins2 = models.IntegerField(default=0)
-    channel = models.ForeignKey(TwitchUser, null=True)
+    channel = models.ForeignKey(TwitchUser, null=True, on_delete=models.PROTECT)
 
     # Null until the match has ended
     ended_at = models.DateTimeField(null=True)
@@ -17,10 +17,10 @@ class Match(models.Model):
         game = Game(match=self, winner=winner, losing_score=losing_score)
         game.save()
 
+
 class Game(models.Model):
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
     winner = models.ForeignKey(User, on_delete=models.PROTECT)
     losing_score = models.IntegerField()
 
     ended_at = models.DateTimeField(auto_now_add=True)
-
