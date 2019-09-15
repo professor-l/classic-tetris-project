154e8cc2cae024cee5802f69903fdee31682196c
Michael Lin <michael@michaelelin.com>
2019-09-15 16:30:43 -0400

    Add !stencil command

--- classic_tetris_project/commands/help.py
+++ classic_tetris_project/commands/help.py
@@ -3,8 +3,14 @@ from ..util import Platform
 
 COMMANDS_URL = "https://github.com/professor-l/classic-tetris-project/blob/master/COMMANDS.md"
 
-@Command.register("help",
-                  usage="help")
+@Command.register("help", usage="help")
 class HelpCommand(Command):
     def execute(self, *args):
         self.send_message(f"All available commands are documented at {COMMANDS_URL}")
+
+
+@Command.register("stencil", usage="stencil")
+class StencilCommand(Command):
+    def execute(self, *args):
+        self.send_message("The stencil helps the streamer line up your Tetris playfield with their "
+                          "broadcast scene. Link here: http://bit.ly/TheStencil")
