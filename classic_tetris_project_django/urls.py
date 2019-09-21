72185d17ceb5bd2174a37cf2afa395c5882256a6
Michael Lin <michael@michaelelin.com>
2019-09-21 05:20:40 +0000

    Add placeholder index.html

--- classic_tetris_project_django/urls.py
+++ classic_tetris_project_django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ]
