b2264631366bd19c4c9273f3811d8964ec23de41
alex-ong <the.onga@gmail.com>
2020-03-22 11:45:46 +1100

    Added a few helper functions and got latest reportmatchmodule

--- classic_tetris_project/commands/reportmatch.py
+++ classic_tetris_project/commands/reportmatch.py
@@ -1,11 +1,27 @@
 from .command import Command, CommandException
 from .. import discord
 from datetime import datetime
-from ..reportmatchmodule.processrequest import processRequest
+from ..reportmatchmodule.processrequest import processRequest, updateChannel, setupChannel
 
 @Command.register_discord_aj("reportmatch", "yadda yadaa", usage="reportmatch, yadda yadda")
 class ReportMatch(Command):
-    def execute(self, *args):        
+    def execute(self, *args):
+        if len(args) > 1 and args[0] == "setup": #hackerman...
+            self.executeSetup(args)
+        else:
+            self.executePeon(args)
+
+    def executePeon(self, *args):
         print (self.context.message.content)
-        result = processRequest(self.context.message.content)
+        league, result = processRequest(self.context.message.content)
         self.send_message("```" + result + "```")
+        if league is not None:
+            self.executeUpdate(league)
+
+    # :redheart: setup cc
+    def executeSetup(self, *args):        
+        league = args[0][1]
+        setupChannel(self.context, league)
+        
+    def executeUpdate(self, league):
+        print ("updating the channel image etc.")
--- classic_tetris_project/discord.py
+++ classic_tetris_project/discord.py
@@ -9,3 +9,6 @@ logger = logging.getLogger("discord-bot")
 
 def get_guild():
     return client.get_guild(guild_id)
+
+def get_channel(id):
+    return client.get_channel(id)
