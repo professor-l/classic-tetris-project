ce88c4a0fc8495bd919176b08f7ba09d3703b6ab
alex-ong <the.onga@gmail.com>
2020-03-23 17:49:14 +1100

    Blacked reportmatch.py

--- classic_tetris_project/commands/reportmatch.py
+++ classic_tetris_project/commands/reportmatch.py
@@ -1,29 +1,33 @@
 from .command import Command, CommandException
 from .. import discord
 from datetime import datetime
-from ..reportmatchmodule.processrequest import processRequest, updateChannel, setupChannel
+from ..reportmatchmodule.processrequest import (
+    processRequest,
+    updateChannel,
+    setupChannel,
+)
+
 
 @Command.register_discord("reportmatch", "Not sure what goes here", usage="reportmatch, yadda yadda")
 class ReportMatch(Command):
     def execute(self, *args):
-        if len(args) > 1 and args[0] == "setup": #hackerman...
+        if len(args) > 1 and args[0] == "setup":  # hackerman...
             self.check_moderator()
             self.executeSetup(args)
         else:
             self.executePeon(args)
 
     def executePeon(self, *args):
-        print (self.context.message.content)
         league, result = processRequest(self.context.message.content)
         self.send_message("```" + result + "```")
         if league is not None:
             self.executeUpdate(league)
 
     # :redheart: setup cc
-    def executeSetup(self, *args):        
+    def executeSetup(self, *args):
         league = args[0][1]
         setupChannel(self.context, league)
-        
+
     def executeUpdate(self, league):
-        print ("Updating the channel image etc.")
+        print("Updating the channel image etc.")
         updateChannel(self.context, league)
