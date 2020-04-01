9c032d0f1509caffc7452d24a53f28129a8648c0
Elle Nolan <ellemnolan@gmail.com>
2020-04-01 04:54:35 +0000

    Add sourcecode command and mod-only stats command

--- classic_tetris_project/commands/info.py
+++ classic_tetris_project/commands/info.py
@@ -1,12 +1,17 @@
 from .command import Command
 
 COMMANDS_URL = "https://github.com/professor-l/classic-tetris-project/blob/master/docs/COMMANDS.md"
+SOURCECODE_URL = "https://github.com/professor-l/classic-tetris-project"
 
-@Command.register("help", usage="help")
+@Command.register("help", "commands", usage="help")
 class HelpCommand(Command):
     def execute(self, *args):
         self.send_message(f"All available commands are documented at {COMMANDS_URL}")
 
+@Command.register("code", "source", "sourcecode", usage="source")
+class SourceCodeCommand(Command):
+    def execute(self, *args):
+        self.send_message(f"The source code for this bot can be found here: {SOURCECODE_URL}")
 
 @Command.register("stencil", usage="stencil")
 class StencilCommand(Command):
