2683edcf31c6858fafa8c15b65db26116729a6f2
Michael Lin <michael@michaelelin.com>
2020-04-02 01:29:10 -0400

    Add WebsiteUser model, move stuff into private

--- classic_tetris_project/admin.py
+++ classic_tetris_project/admin.py
@@ -1,35 +1,5 @@
-from django.contrib import admin
-
-from .models import User, DiscordUser, TwitchUser, Match, Game, TwitchChannel, CustomCommand
-
-
-class DiscordUserInline(admin.StackedInline):
-    model = DiscordUser
-
-class TwitchUserInline(admin.StackedInline):
-    model = TwitchUser
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    inlines = [DiscordUserInline, TwitchUserInline]
-
-admin.site.register(DiscordUser)
-admin.site.register(TwitchUser)
-
-
-class GameInline(admin.TabularInline):
-    model = Game
-
-@admin.register(Match)
-class MatchAdmin(admin.ModelAdmin):
-    inlines = [GameInline]
-    list_display = ('__str__', 'player1', 'wins1', 'player2', 'wins2', 'channel', 'ended_at')
-
-
-class CustomCommandInline(admin.TabularInline):
-    model = CustomCommand
-
-@admin.register(TwitchChannel)
-class TwitchChannelAdmin(admin.ModelAdmin):
-    inlines = [CustomCommandInline]
-    list_display = ("__str__", "twitch_user", "connected")
+try:
+    from .private.admin import *
+except ModuleNotFoundError:
+    # private not loaded, ignore
+    pass
--- classic_tetris_project/commands/reportmatch.py
+++ classic_tetris_project/commands/reportmatch.py
@@ -1,22 +1,27 @@
 from .command import Command, CommandException
 from .. import discord
 from datetime import datetime
-from ..reportmatchmodule.processrequest import (
-    processRequest,
-    updateChannel,
-    setupChannel,
-    checkChannelPeon
-)
+try:
+    from ..reportmatchmodule.processrequest import (
+        processRequest,
+        updateChannel,
+        setupChannel,
+        checkChannelPeon
+    )
+    REPORT_MATCH_LOADED = True
+except ModuleNotFoundError:
+    REPORT_MATCH_LOADED = False
 
 
 @Command.register_discord("reportmatch", usage="reportmatch, yadda yadda")
 class ReportMatch(Command):
     def execute(self, *args):
-        if len(args) > 1 and args[0] == "setup":  # hackerman...
-            self.check_moderator()
-            self.executeSetup(args)
-        else:
-            self.executePeon(args)
+        if REPORT_MATCH_LOADED:
+            if len(args) > 1 and args[0] == "setup":  # hackerman...
+                self.check_moderator()
+                self.executeSetup(args)
+            else:
+                self.executePeon(args)
 
     def executePeon(self, *args):
         # only accept reports in the reporting channel
