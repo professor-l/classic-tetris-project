1d3e05bae97f97ec962358c9f09926f5abc3d08c
alex-ong <the.onga@gmail.com>
2020-04-24 03:26:01 +1000

    Working animation.

--- classic_tetris_project/util/fieldgen/field_generator.py
+++ classic_tetris_project/util/fieldgen/field_generator.py
@@ -9,6 +9,8 @@ from .gravity import GravityFrames
 from .level import LevelGenerator
 from .input import InputGenerator
 from .ai import Aesthetics
+from .activepiece import ActivePieceGenerator
+
 
 class FieldGenerator(object):
     TILE_MANAGER = TileManager(AssetPath.get_asset_root())
@@ -25,7 +27,7 @@ class FieldGenerator(object):
         if InputGenerator.input_too_long(self.sequence):
             return bc.export_bytearray()
         
-        #self.simulate_game(bc, level, height, sequence)
+        self.simulate_game(bc)
         return bc.export_bytearray()
 
     def generate_base(self, canvas):
@@ -41,42 +43,38 @@ class FieldGenerator(object):
     
     def simulate_game(self, canvas):
         gravity_frames = GravityFrames.get_gravityframes(self.level)
-
-        shifts = 0
-        g_counter = 0            
-        column_dir = Aesthetics.get_piece_shift_direction(self.sequence)
-        
+        column_dir = Aesthetics.get_piece_shift_direction(self.sequence)        
         seq_length = InputGenerator.input_length(self.sequence)
-        anim_length = seq_length + 3 * gravity
+        anim_length = seq_length + 3 * gravity_frames
         stop_grav = None
-        if height < GarbageGenerator.TETRIS_HEIGHT:
-            stop_grav = anim_length - (4 - height)*gravity
+        if self.height < GarbageGenerator.TETRIS_HEIGHT:
+            stop_grav = anim_length - (4 - height)*gravity_frames
         
         ig = InputGenerator(self.TILE_MANAGER)
-        #ap = ActivePieceGenerator(self.TILE_MANAGER)
+        ap = ActivePieceGenerator(self.TILE_MANAGER)
 
+        shifts = 0
+        g_counter = 0
         for i in range(anim_length):
-            new_frame = bc.clone_baseimg()
+            new_frame = canvas.clone_baseimg()
         
             # input display
-            ig.draw_input_red(new_frame, i, sequence)
+            ig.draw_input_red(new_frame, i, self.sequence)
             
-            if i in sequence:            
+            # perform the simulation
+            if i in self.sequence:            
                 shifts += 1
             
             if stop_grav is None or g_counter < stop_grav:
                 g_counter += 1
             
-            row_offset = 2 + g_counter // gravity
+            # calculate long bar position
+            piece_x = 5 + shifts * column_dir
+            piece_y = 0 + g_counter // gravity_frames
             
-            # I piece paster
-            piece_x = 6 + shifts * column_dir
-            for j in range(4):
-                row = row_offset - j
-                if row >= 1:
-                    new_frame.paste(block,(get_block_rect([piece_x,row])))
+            ap.draw_longbar(new_frame, (piece_x, piece_y), self.level)
 
-            bc.add_frame(new_frame)
+            canvas.add_frame(new_frame)
     
 
 def simulate_game(bc, level,height,sequence):
--- classic_tetris_project/util/fieldgen/level.py
+++ classic_tetris_project/util/fieldgen/level.py
@@ -4,10 +4,10 @@ class LevelGenerator(object):
         self.tile_gen = tile_gen
     
     def draw_level(self, image, level):
-        if level <= 99:        
-            first = self.tile_gen.get_number_tile(level // 10)
-            second = self.tile_gen.get_number_tile(level % 10)
-            first_coord = TileMath.tile_indices_to_pixels([15,16])
-            second_coord = TileMath.tile_indices_to_pixels([16,16])
-            image.paste(first, first_coord)
-            image.paste(second, second_coord)
+        level %= 99
+        first = self.tile_gen.get_number_tile(level // 10)
+        second = self.tile_gen.get_number_tile(level % 10)
+        first_coord = TileMath.tile_indices_to_pixels([15,16])
+        second_coord = TileMath.tile_indices_to_pixels([16,16])
+        image.paste(first, first_coord)
+        image.paste(second, second_coord)
