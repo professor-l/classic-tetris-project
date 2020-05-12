e993a728dc9b49795799f0d09ff2691373adba78
Michael Lin <michael@michaelelin.com>
2020-05-12 19:04:47 -0400

    Add simulations view

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 from .views import oauth
+from .views import simulations
 from .views.index import index
 
 oauth_patterns = ([
@@ -9,7 +10,12 @@ oauth_patterns = ([
     path("logout/", oauth.logout, name="logout"),
 ], "oauth")
 
+simulations_patterns = ([
+    path("hz.gif", simulations.HzView.as_view(), name="hz"),
+], "simulations")
+
 urlpatterns = [
     path("", index, name="index"),
     path("oauth/", include(oauth_patterns)),
+    path("simulations/", include(simulations_patterns)),
 ]
