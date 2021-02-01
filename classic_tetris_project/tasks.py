c83953d73635c50efb1000695d1434e783923c79
Michael Lin <michael@michaelelin.com>
2021-02-01 03:39:52 -0500

    Announce qualifier to Discord

--- classic_tetris_project/tasks.py
+++ classic_tetris_project/tasks.py
@@ -13,6 +13,43 @@ QUALIFIER_COLOR_REJECTED = 0xE60000
 
 
 @shared_task
+def announce_qualifier(qualifier_id):
+    qualifier = Qualifier.objects.get(id=qualifier_id)
+    event = qualifier.event
+    discord_user = qualifier.user.discord_user
+    twitch_user = qualifier.user.twitch_user
+
+    # Send message to channel
+    channel_id = env("DISCORD_QUALIFIER_REPORTING_CHANNEL_ID")
+    if channel_id:
+        message = "{discord_user} is about to qualify for {event}! Go watch live on {twitch_url}".format(
+            discord_user=discord_user.user_tag,
+            event=event.name,
+            twitch_url=twitch_user.twitch_url,
+        )
+        discord.send_channel_message(channel_id, message)
+
+
+@shared_task
+def report_submitted_qualifier(qualifier_id):
+    qualifier = Qualifier.objects.get(id=qualifier_id)
+    event = qualifier.event
+    discord_user = qualifier.user.discord_user
+
+    # Send message to channel
+    channel_id = env("DISCORD_QUALIFIER_REPORTING_CHANNEL_ID")
+    if channel_id:
+        channel_embed = MessageEmbed()
+        channel_embed.description = render_to_string("discord/qualifier_submitted.txt", {
+            "event_name": event.name,
+            "event_url": event.get_absolute_url(True),
+            "qualifier": qualifier,
+        }).replace("\\\n", "").strip()
+        channel_embed.color = QUALIFIER_COLOR_APPROVED
+        discord.send_channel_message(channel_id, embed=channel_embed)
+
+
+@shared_task
 def report_reviewed_qualifier(qualifier_id):
     qualifier = Qualifier.objects.get(id=qualifier_id)
     event = qualifier.event
@@ -20,7 +57,7 @@ def report_reviewed_qualifier(qualifier_id):
 
     # Send message to user
     user_embed = MessageEmbed()
-    user_embed.description = render_to_string("discord/qualifier_reviewed_user.txt", {
+    user_embed.description = render_to_string("discord/qualifier_reviewed.txt", {
         "event_name": event.name,
         "event_url": event.get_absolute_url(True),
         "qualifier": qualifier,
@@ -30,17 +67,4 @@ def report_reviewed_qualifier(qualifier_id):
                    if qualifier.review_data["checks"] else None),
     }).replace("\\\n", "").strip()
     user_embed.color = (QUALIFIER_COLOR_APPROVED if qualifier.approved else QUALIFIER_COLOR_REJECTED)
-
     discord.send_direct_message(discord_user.discord_id, embed=user_embed)
-
-    # Send message to channel
-    channel_id = env("DISCORD_QUALIFIER_REPORTING_CHANNEL_ID")
-    if qualifier.approved and channel_id:
-        channel_embed = MessageEmbed()
-        channel_embed.description = render_to_string("discord/qualifier_reviewed_channel.txt", {
-            "event_name": event.name,
-            "event_url": event.get_absolute_url(True),
-            "qualifier": qualifier,
-        }).replace("\\\n", "").strip()
-        channel_embed.color = QUALIFIER_COLOR_APPROVED
-        discord.send_channel_message(channel_id, embed=channel_embed)
