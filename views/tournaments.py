7b2f57517124e295c213a605fea4019bf59e19db
Fireworks <justin@mysticrealms.net>
2022-03-04 01:43:08 -0700

    Functional prototype of a bracket visualization on tournament pages

--- views/tournaments.py
+++ views/tournaments.py
@@ -17,6 +17,16 @@ class TournamentView(EventView):
 
 
 class IndexView(TournamentView):
+
+    @staticmethod
+    def generate_round(match_count, all_matches, bracket_matches):
+        new_matches = []
+        for _ in range(match_count):
+            next_match = all_matches.pop(0)
+            new_matches.append(next_match)
+
+        return new_matches
+
     def get(self, request, event_slug, tournament_slug):
         if not self.tournament.public:
             raise Http404()
@@ -30,9 +40,20 @@ class IndexView(TournamentView):
         playable_matches = [match_display for match_display in all_matches
                             if match_display.tournament_match.is_playable()]
 
+        player_count = len(all_players)
+        bracket_matches = []
+        all_matches_copy = all_matches.copy()
+
+        while player_count > 1:
+            player_count //= 2
+            bracket_matches.append(IndexView.generate_round(player_count, all_matches_copy, bracket_matches))
+
+        print("bracket_matches", bracket_matches)
+
         return render(request, "tournament/index.haml", {
             "tournament": self.tournament,
             "tournament_players": all_players,
             "tournament_matches": all_matches,
             "playable_matches": playable_matches,
+            "bracket_matches": bracket_matches,
         })
