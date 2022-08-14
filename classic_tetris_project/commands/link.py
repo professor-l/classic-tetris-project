991d2126f9846dfdc7de5d286f6a68ec4ecb1f98
Fireworks <justin@mysticrealms.net>
2022-08-13 21:07:05 -0600

    Update CTM website link in link command

--- classic_tetris_project/commands/link.py
+++ classic_tetris_project/commands/link.py
@@ -3,4 +3,4 @@ from .command import Command, CommandException
 @Command.register("link", "linkaccount", usage="link")
 class LinkCommand(Command):
     def execute(self, *args):
-        self.send_message("To link your Twitch or Discord account, head to our website and click 'Login' at the top right: https://monthlytetris.info.  Once there, you can view and edit your profile.")
+        self.send_message("To link your Twitch or Discord account, head to our website and click 'Login' at the top right: https://go.ctm.gg.  Once there, you can view and edit your profile.")
